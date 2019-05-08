751936fd86b2715cafb5a4698d8a4ec634aec243
Ola Zych <aleksandra.m.zych@gmail.com>
2019-05-08 21:09:38 +0200

    Assignment week 3

--- Homework/variables_homework/src/Week_2_assignment.java
+++ Homework/variables_homework/src/Week_2_assignment.java
@@ -1,33 +1,47 @@
+import java.util.Scanner;
 public class Week_2_assignment {
     public static void main(String[] args) {
-        String item_1_name = "Item1";
-        String item_2_name = "Item2";
-        int item_1_price= 309;
-        int item_2_price=71;
-        int item_1_quantity=1;
-        int item_2_quantity=1;
-        int total_cost_1 = item_1_price*item_1_quantity;
-        int total_cost_2 = item_2_price*item_2_quantity;
-        int item_1_coupon = 20;
-        int item_2_coupon = 15;
-        int final_cost_1 = total_cost_1 - ((total_cost_1*item_1_coupon)/100);
-        int final_cost_2 = total_cost_2-((total_cost_2*item_2_coupon)/100);
+        Scanner console = new Scanner(System.in);
+        String item_1_name;
+        String item_2_name ;
+        double item_1_price;
+        double item_2_price;
+        int item_1_quantity;
+        int item_2_quantity;
+        int item_1_coupon ;
+        int item_2_coupon ;
+
+
+        System.out.println("Enter the name of item: ");
+        item_1_name = console.next();
+        System.out.println("Enter the price of "+ item_1_name);
+        item_1_price = console.nextDouble();
+        System.out.println("Enter the quantity: ");
+        item_1_quantity = console.nextInt();
+        double total_cost_1 = item_1_price*item_1_quantity;
+        System.out.println("Enter the name of next item: ");
+        item_2_name = console.next();
+        System.out.println("Enter the price of "+ item_2_name);
+        item_2_price = console.nextDouble();
+        System.out.println("Enter the quantity: ");
+        item_2_quantity = console.nextInt();
+        double total_cost_2 = item_2_price*item_2_quantity;
         System.out.println("Total cost of "+item_1_name +" is "+ total_cost_1);
         System.out.println("Total cost of "+item_2_name +" is "+ total_cost_2);
+
+        System.out.println("Enter the coupon for (as a %)" + item_1_name);
+        item_1_coupon = console.nextInt();
+        System.out.println("Enter the coupon for (as a %)" + item_2_name);
+        item_2_coupon = console.nextInt();
+
+
+
+        double final_cost_1 = total_cost_1 - ((total_cost_1*item_1_coupon)/100);
+        double final_cost_2 = total_cost_2-((total_cost_2*item_2_coupon)/100);
+
         System.out.println("Final cost of "+item_1_name +" is "+final_cost_1);
         System.out.println("Final cost of "+item_2_name +" is " + final_cost_2);
 
-        //with double variables
-        double item_1_price_d = 309.00;
-        double item_2_price_d = 71.00;
-        double total_cost_1_d = item_1_price_d*item_1_quantity;
-        double total_cost_2_d = item_2_price_d*item_2_quantity;
-        double final_cost_1_d = total_cost_1 - ((total_cost_1_d*item_1_coupon)/100);
-        double final_cost_2_d = total_cost_2-((total_cost_2_d*item_2_coupon)/100);
-        System.out.println("Accurate total cost of "+item_1_name +" is "+ total_cost_1_d);
-        System.out.println("Accurate total cost of "+item_2_name +" is "+ total_cost_2_d);
-        System.out.println("Accurate final cost of "+item_1_name +" is "+final_cost_1_d);
-        System.out.println("Accurate final cost of "+item_2_name +" is "+ final_cost_2_d);
 
     }
 }
